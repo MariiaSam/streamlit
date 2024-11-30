91c8c5493b9bf1f91f718d74a70c965d39f241b1
MariiaSam <kontragent@ukr.net>
2024-11-30 19:03:33 +0200

    corr

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-try:
-    import cv2
-except ImportError as e:
-    import streamlit as st
-    st.error(f"Не вдалося імпортувати OpenCV: {e}")
-    raise
-import cv2
-from PIL import Image
+from PIL import Image, ImageOps  
 
 @st.cache_resource
 def load_models():
-    model1 = tf.keras.models.load_model("model_name.h5") 
-    model2 = tf.keras.models.load_model("model_vgg16.h5")  
+    model1 = tf.keras.models.load_model("model_name.h5")
+    model2 = tf.keras.models.load_model("model_vgg16.h5")
     return model1, model2
 
 model1, model2 = load_models()
 
 def preprocess_image(image):
-    image = np.array(image)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
-    resized_image = cv2.resize(gray_image, (28, 28)) 
-    normalized_image = resized_image / 255.0  
+    gray_image = ImageOps.grayscale(image)  
+    resized_image = gray_image.resize((28, 28))  
+    normalized_image = np.array(resized_image) / 255.0  
     input_image = np.expand_dims(normalized_image, axis=(0, -1))  
     return input_image
 
